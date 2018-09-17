6d71a397e57aef9c7bf73c25fd0409cee67f528c
zlw11063 <wil11063@byui.edu>
2018-09-16 21:22:28 -0600

    Basic testing

--- exArrays.js
+++ exArrays.js
@@ -18,7 +18,8 @@ function mean(arr) {
     return sum / arr.length;
 }
 
-let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
-
-console.log(evens(array));
-console.log(mean(array));
+module.exports = {
+    evens,
+    oddIndexes,
+    mean
+}
--- test/test.js
+++ test/test.js
@@ -1,7 +1,16 @@
 var tap = require('tap');
-var exArray = require('../firstMethods.js');
+var exArray = require('../exArrays.js');
 
-var testArr = [1, 5, 7, 23, 4];
+var testArr = [1, 5, 7, 6, 23, 4, 3];
 
-console.log(exArray.evens(testArr))
-tap.equal(exArray.evens(testArr), [4]);
+tap.test('method:evens', test => {
+    tap.equal(exArray.evens(testArr)[0], 6);
+    tap.equal(exArray.evens(testArr)[1], 4);
+    tap.equal(exArray.evens(testArr).length, 2);
+    test.end();
+});
+
+tap.test('method:mean', test => {
+    tap.equal(exArray.mean(testArr), 7);
+    test.end();
+});
